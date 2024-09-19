72cc96c02ba3e0537e35b3ca9ff425714c34527b
Gabriel Bergamini <gabrielbergamini05@gmail.com>
2024-09-18 22:01:32 -0300

    Vetores

---
+++ Estudos C#/CadastroBanco/CadastroBanco/Conta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroBanco
+{
+    class Conta
+    {
+        private int NumeroConta;
+        public string NomeTitular { get; private set; }
+        private int DepInicial { get; set; }
+
+
+
+        public Conta()
+        {
+
+        }
+
+        public Conta(int numeroConta, string nomeTitular, int depInicial)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            DepInicial = depInicial;
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+    }
+
+
+
+

67bbe88a7610bd1f91378c2312a93721b426ab93
Gabriel Bergamini <gabrielbergamini05@gmail.com>
2024-09-18 23:02:28 -0300

    Exercicio Vetores

---
+++ Estudos C#/ExercicioVetores/ExercicioVetores/Livros.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ExercicioVetores
+{
+    internal class Livros
+
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+
+
+        public Livros(string titulo, string autor)
+        {
+            Titulo = titulo;
+            Autor = autor;
+        }
+
+        public override string ToString()
+        {
+            return Titulo + ", " + Autor;
+        }
+    }
+}
+
+
---
+++ Estudos C#/ExercicioVetores/ExercicioVetores/Program.cs
@@ -0,0 +1,45 @@
+﻿using ExercicioVetores;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Livros[] vect = new Livros[10];
+            Console.Write("Quantos quartos serão alugados?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                Console.WriteLine("Aluguel #1");
+                Console.Write("Nome: ");
+                string titulo = (Console.ReadLine());
+                Console.Write("Autor: ");
+                string autor = (Console.ReadLine());
+                Console.Write("Estante: ");
+                int estante = int.Parse(Console.ReadLine());
+                vect[estante] = new Livros(titulo, autor);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+
+
+
+
+
+
+
+                }
+            }
+        }
+    }
+}
+    
